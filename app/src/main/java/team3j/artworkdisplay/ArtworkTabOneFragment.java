346a337736d7d5541eef98470a29bb134f6db09f
Jagveer Gill <jagveergill@hotmail.co.uk>
2015-02-08 16:52:37 +0000

    no descriptions

--- app/src/main/java/team3j/artworkdisplay/ArtworkTabOneFragment.java
+++ app/src/main/java/team3j/artworkdisplay/ArtworkTabOneFragment.java
@@ -7,6 +7,9 @@ import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,7 +19,13 @@ import android.widget.TextView;
 
 import com.etsy.android.grid.util.DynamicHeightImageView;
 
+import it.neokree.materialtabs.MaterialTab;
+import it.neokree.materialtabs.MaterialTabHost;
+import team3j.dulwichstreetart.ArtistListFragment;
 import team3j.dulwichstreetart.GalleryData;
+import team3j.dulwichstreetart.GalleryFragment;
+import team3j.dulwichstreetart.HomePageFragment;
+import team3j.dulwichstreetart.MapFragment;
 import team3j.dulwichstreetart.R;
 
 
@@ -30,6 +39,8 @@ public class ArtworkTabOneFragment extends Fragment {
     int indexOfArtWork;
     private DynamicHeightImageView dynamicHeightImageView;
     private Toolbar toolbar;
+    private MaterialTabHost tabHost;
+    private ViewPager viewPager;
 
     public static ArtworkTabOneFragment getInstance(int position, int indexOfArtWork) {
         ArtworkTabOneFragment myFragmentTab = new ArtworkTabOneFragment();
@@ -48,6 +59,9 @@ public class ArtworkTabOneFragment extends Fragment {
         textView = (TextView) layout.findViewById(R.id.position);
         dynamicHeightImageView= (DynamicHeightImageView) layout.findViewById(R.id.dynamic_imageview_artwork_display);
 
+
+        tabHost = (MaterialTabHost) layout.findViewById(R.id.tab_host_artwork);
+        viewPager = (ViewPager) layout.findViewById(R.id.view_pager_artwork);
 
         //get arguments passed in and handle
         Bundle bundle = getArguments();
@@ -70,7 +84,102 @@ public class ArtworkTabOneFragment extends Fragment {
 
         }
 
+        //update tabs
+//        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(),recycleViewScrollViewListener);
+//        viewPager.setAdapter(adapter);
+//        viewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+//            @Override
+//            public void onPageSelected(int position) {
+//                tabHost.setSelectedNavigationItem(position);
+//            }
+//        });
+//
+//        for (int i = 0; i < adapter.getCount(); i++) {
+//            MaterialTab materialTab=
+//                    tabHost.newTab()
+//                            .setText(adapter.getPageTitle(i))
+//                            .setTabListener(this);
+//            tabHost.addTab(materialTab);
+//
+//            materialTab.setTextColor(getResources().getColor(R.color.colorAccent));
+//
+//
+//        }
+
 
         return layout;
     }
+
+
+    private class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+        private GalleryFragment.RecycleViewScrollViewListener recycleViewScrollViewListener;
+
+
+        public ViewPagerAdapter(FragmentManager fragmentManager, GalleryFragment.RecycleViewScrollViewListener recycleViewScrollViewListener) {
+            super(fragmentManager);
+
+            this.recycleViewScrollViewListener=recycleViewScrollViewListener;
+        }
+
+
+        public Fragment getItem(int num) {
+            /*
+            returns a fragment to place in the tab
+             */
+
+
+            switch(num){
+                case 0:
+//                    HomePageFragment homePageFragment=  HomePageFragment.getInstance(num);
+                    //add send through the interface here.
+                    // homePageFragment.addScrollListerner();
+
+
+
+
+
+
+                    return HomePageFragment.getInstance(num);
+
+                case 1:
+                    GalleryFragment galleryFragment= GalleryFragment.getInstance(num);
+                    galleryFragment.setRecycleViewScrollViewListener(recycleViewScrollViewListener);
+
+
+                    return galleryFragment;
+                case 2:
+                    return ArtistListFragment.getInstance(num);
+                case 3:
+                    return MapFragment.getInstance(num);
+                case 4:
+                    return MapFragment.getInstance(num);
+
+                default:
+                    return HomePageFragment.getInstance(num);
+
+
+            }
+
+        }
+
+
+        @Override
+        public int getCount() {
+            return 5;
+        }
+
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return getResources().getStringArray(R.array.tabs)[position];
+        }
+
+//        private Drawable getIcon(int position) {
+//            return getResources().getDrawable(icons[position]);
+//        }
+
+
+    }
+
 }
