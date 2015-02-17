285f6d70e2f77b11d89560682b44507c5f6aa69f
Vincent Truong <vincent.truong95@gmail.com>
2015-02-17 14:59:52 +0000

    MERGE THIS

--- app/src/main/java/team3j/artworkdisplay/GallerySwipeSingleFragment.java
+++ app/src/main/java/team3j/artworkdisplay/GallerySwipeSingleFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
