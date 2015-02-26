201d57ae4638dc7c6a2c80feab99e1b0f78bba7d
CheokHo Mai <cheokho@hotmail.co.uk>
2015-02-26 16:00:01 +0000

    Updated to facebook style timestamp. Timestamp displayed depends on post time.

--- app/src/main/java/team3j/artworkdisplay/CommentListAdapter.java
+++ app/src/main/java/team3j/artworkdisplay/CommentListAdapter.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,15 @@ import android.widget.TextView;
 import com.etsy.android.grid.util.DynamicHeightImageView;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.EnumSet;
+import java.util.GregorianCalendar;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import team3j.dulwichstreetart.GalleryData;
 import team3j.dulwichstreetart.R;
@@ -96,9 +106,40 @@ public class CommentListAdapter extends RecyclerView.Adapter<CommentListAdapter.
             Comment commentInfo = data.get(position-1);
             holder.posterName.setText(commentInfo.getPosterName());
             holder.message.setText(commentInfo.getMessage());
-            holder.timestamp.setText(commentInfo.getTime());
-
-
+            int year = Integer.parseInt(commentInfo.getTime().substring(0, 4));
+            int month = Integer.parseInt(commentInfo.getTime().substring(5, 7))-1;
+            int day = Integer.parseInt(commentInfo.getTime().substring(8, 10));
+            int hour = Integer.parseInt(commentInfo.getTime().substring(11, 13));
+            int minute = Integer.parseInt(commentInfo.getTime().substring(14, 16));
+            System.out.println("TEEEEEEEEEEEEST   "+year+"   "+month+"   "+day+"   "+hour+"   "+minute);
+            Calendar postDate = GregorianCalendar.getInstance();
+            postDate.set(year, month, day, hour, minute);
+            Map<TimeUnit, Long> timeSincePost = getTimeDifference(postDate.getTime(), new Date()); // new Date = current
+            System.out.println(commentInfo.getTime());
+            System.out.println("MONTH "+month+"  HOUR "+hour+" MINUTE "+minute);
+            System.out.println(postDate.getTime());
+            if (timeSincePost.get(TimeUnit.DAYS) > 0 && timeSincePost.get(TimeUnit.DAYS) < 8) {
+                holder.timestamp.setText(timeSincePost.get(TimeUnit.DAYS)+ " days ago");
+            }
+            else if (timeSincePost.get(TimeUnit.DAYS) > 7) {
+                holder.timestamp.setText(postDate.getTime().toString().substring(4,10)+ " at "+ postDate.getTime().toString().substring(11,16));
+            }
+            else if (timeSincePost.get(TimeUnit.DAYS) == 0) {
+                if (timeSincePost.get(TimeUnit.HOURS) == 1) {
+                    holder.timestamp.setText("About one hour ago");
+                }
+                else if (timeSincePost.get(TimeUnit.HOURS) > 1) {
+                    holder.timestamp.setText(timeSincePost.get(TimeUnit.HOURS)+ " hours ago");
+                }
+                else if (timeSincePost.get(TimeUnit.HOURS) ==0) {
+                    if (timeSincePost.get(TimeUnit.MINUTES) == 1){
+                        holder.timestamp.setText("About one minute ago");
+                    }
+                    else {
+                        holder.timestamp.setText(timeSincePost.get(TimeUnit.MINUTES)+ " minutes ago");
+                    }
+                }
+            }
         }
 
     }
@@ -110,6 +151,23 @@ public class CommentListAdapter extends RecyclerView.Adapter<CommentListAdapter.
         }else{
             return data.size()+1;
         }
+    }
+
+    //Calculates difference between two dates and returns as Map containing keys for DAY, HOURS, MINUTES, SECONDS, MILLISECONDS etc etc...
+    public Map<TimeUnit, Long> getTimeDifference(Date date1, Date date2) {
+        long diffInMill = date2.getTime() - date1.getTime();
+        List<TimeUnit> units = new ArrayList<TimeUnit>(EnumSet.allOf(TimeUnit.class));
+        Collections.reverse(units);
+
+        Map<TimeUnit, Long> result = new LinkedHashMap<TimeUnit, Long>();
+        long millRest = diffInMill;
+        for (TimeUnit unit : units) {
+            long diff = unit.convert(millRest, TimeUnit.MILLISECONDS);
+            long diffInMilliesForUnit = unit.toMillis(diff);
+            millRest = millRest - diffInMilliesForUnit;
+            result.put(unit, diff);
+        }
+        return result;
     }
 
 
