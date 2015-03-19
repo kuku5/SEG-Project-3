33d621dff3df28fb3c0eb25f2795492066e71a6d
Vincent Truong <vincent.truong95@gmail.com>
2015-03-19 23:16:38 +0000

    Art class, javadocs 95% only constructor left

--- app/src/main/java/team3j/dulwichstreetart/Art.java
+++ app/src/main/java/team3j/dulwichstreetart/Art.java
@@ -23,6 +23,7 @@ public class Art {
     private Boolean visited;
 
     /**
+     * main constructor passing through art information -
      *  @param name
      * @param artistName
      * @param inspirationTitle
@@ -36,9 +37,6 @@ public class Art {
      * @param visited
      * @param webLinks
      */
-
-
-    //main constructor passing through art information -
 
     public Art(String name, String artistName, String inspirationTitle, String inspirationArtist, String desc, int pic, int inspiredPic, LatLng loc, String fbLink, String artAddress, Boolean visited, String[] webLinks){
         this.name=name;
@@ -55,45 +53,98 @@ public class Art {
         this.webLinks=webLinks;
     }
 
+    /**
+     * Get the name of the artwork
+     * @return name of artwork
+     */
     public String getName(){
         return name;
     }
 
+    /**
+     * Get the coordinates of the artwork
+     * @return coordinates of the artwork
+     */
     public LatLng getLoc()
     {
         return loc;
     }
 
+    /**
+     * Get the artwork itself
+     * @return the picture of the artwork
+     */
     public int getPic(){
         return pic;
     }
 
+    /**
+     *  Gets the inspiration picture
+     * @return the inspiration artwork
+     */
     public int getInspiredPic() {return inspiredPic;}
 
+    /**
+     * Gets the description of the artwork
+     * @return description of artwork
+     */
     public String getDesc() {return desc;}
 
+    /**
+     * Gets the name of the inspiration artwork
+     * @return name of the inspiration artwork
+     */
     public String getInspirationTitle() {return inspirationTitle;}
 
+    /**
+     * Gets the artist's name
+     * @return the artist's name
+     */
     public String getArtistName() {return artistName;}
 
+    /**
+     * Gets the inspiration artist name
+     * @return inspiration artist name
+     */
     public String getInspirationArtist() { return inspirationArtist;}
 
+    /**
+     * Set the name of the artwork
+     * @param name name of the artwork
+     */
     public void  setName( String name){
         this.name = name;
     }
 
+    /**
+     * Gets the facebook code for the picture
+     * @return
+     */
     public String getFbLink() {return fbLink;}
 
+    /**
+     * Gets the actual address of the artwork
+     * @return
+     */
     public String getArtAddress() { return artAddress;}
 
-
+    /**
+     * Gets if the user has visited the artwork or not
+     * @return true if they have visted
+     */
     public Boolean getVisited() {
         return visited;
     }
 
+    /**
+     * Sets whether they have visited the artwork or not
+     */
     public void setVisited(){this.visited = true;}
 
-
+    /**
+     * Gets the array of extra links for the artist
+     * @return extra links for the artist
+     */
     public String[] getWebLinks() {
         return webLinks;
     }
